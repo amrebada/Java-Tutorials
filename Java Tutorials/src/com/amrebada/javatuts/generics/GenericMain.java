3ca51e5826ede8d2983cad51de5fcc56be5798b3
Amr Ebada <amr.app.engine@gmail.com>
2018-07-30 16:45:36 +0400

    d
    d

---
+++ Java Tutorials/src/com/amrebada/javatuts/generics/GenericMain.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.amrebada.javatuts.generics;
+
+/**
+ *
+ * @author AmrvEbada <amr.ebada@itv-me.com>
+ */
+public class GenericMain {
+    public static void main(String[] args) {
+        
+    }
+}
